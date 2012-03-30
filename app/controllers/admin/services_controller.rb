ef577b8f6adf8963dfa18e3d6f79618828bc2bf2
wei <wei@summers.com.tw>
2012-03-30 18:59:43 +0800

    service ok service menu ok

--- app/controllers/admin/services_controller.rb
+++ app/controllers/admin/services_controller.rb
@@ -15,7 +15,7 @@ layout "admin"
   # GET /system_site_maps/1.json
   def show
     @service = Service.find(params[:id])
-
+    build_service_content
     respond_to do |format|
       format.html # show.html.erb
     end
@@ -25,6 +25,7 @@ layout "admin"
   # GET /system_site_maps/new.json
   def new
     @service = Service.new
+    #build_service_content
     respond_to do |format|
       format.html # new.html.erb
     end
@@ -33,6 +34,7 @@ layout "admin"
   # GET /system_site_maps/1/edit
   def edit
     @service = Service.find(params[:id])
+    build_service_content
   end
 
   # POST /system_site_maps
@@ -42,8 +44,6 @@ layout "admin"
     parent_system_site_map_id = params[:pid]
     
     package_content
-    
-    @service.content = content
     
     respond_to do |format|
       if @service.save# && @system_site_map.save
@@ -98,5 +98,11 @@ private
 	    contents = params[:contents]	    
 	    @service.content = ActiveSupport::JSON.encode(contents	)
 	    
-	 end 
+	 end
+
+ 	def build_service_content
+
+ 		@service.content = JSON.parse(@service.content)
+
+ 	end
 end
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,9 +1,9 @@
 module AdminHelper
   def menu(text, controller, action='index', pid=nil)
-    if params[:controller].split('/').last == controller
-      return link_to text, {controller: controller, action: action}, :class=>'active'
+    if params[:controller].split('/').last == controller && params[:pid].to_s == pid.to_s
+      return link_to text, {controller: controller, action: action, pid: pid}, :class=>'active'
     else
-        return link_to text, {controller: controller, action: action}
+        return link_to text, {controller: controller, action: action, pid: pid}
     end
   end
 end
