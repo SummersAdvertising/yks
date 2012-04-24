ce889483391324f905f8ccfce3fe37d777fbe2fd
育哲 <yuzhe.c85@gmail.com>
2012-04-24 15:50:12 +0800

    修正preload與asset pipeline錯誤

--- app/controllers/banners_controller.rb
+++ app/controllers/banners_controller.rb
@@ -2,10 +2,11 @@ class BannersController < ApplicationController
   layout :except => [:index]
 	def index
 		 @banners = Banner.all
-		 
+
 		 respond_to do |format|
 		 	format.html
 		 end
 		 
 	end
+		
 end
