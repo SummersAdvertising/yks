fef50d26501211e1a4015cda90190e178d283e22
育哲 <yuzhe.c85@gmail.com>
2012-04-24 16:03:52 +0800

    重新merge

--- app/assets/javascripts/jquery.preload.js
+++
@@ -1,7 +0,0 @@
-function preloadImages() {
- var a = (typeof arguments[0] == 'object')? arguments[0] : arguments;
- for(var i = a.length -1; i > 0; i--) {
-  jQuery("<img>").attr("src", 'images/'+a[i]);
- }
-}
-preloadImages("btn_click.png","btn_out.png","btn_over.png","businessbg.png","businessend.png","businesstopbg.png","close.png","contactbg.png","menubg.png","menubg2.png","menubgend.png","menuover.png","needbtn.png","needbtn2.png","open.png","slider.png");
