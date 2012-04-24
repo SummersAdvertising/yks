35f42062b1850cc91a72a64102af5f013f8b1035
育哲 <yuzhe.c85@gmail.com>
2012-04-24 18:18:21 +0800

    Preload 圖片路徑修改

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Yks::Application.initialize!
 
-ENV['RAILS_RELATIVE_URL_ROOT'] = '/'
+ENV['RAILS_RELATIVE_URL_ROOT'] = '/yks'
